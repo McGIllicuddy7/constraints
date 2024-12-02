f7bd3fc03cd024f3e87a5fbeaa070249ac5367db
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-12-02 11:52:04 -0800

    broken

--- src/constraints.rs
+++ src/constraints.rs
@@ -4,34 +4,34 @@ use rand::{thread_rng, RngCore};
 use crate::utils;
 use crate::utils::OFFSETS;
 use crate::utils::MINUS_INDICES;
-
+type TileType = i32;
 
 #[derive(Clone, Debug)]
 pub struct Grid{
-    values:Box<[i16]>, 
+    values:Box<[TileType]>, 
     height:usize, 
     width:usize,
 }
 
 impl Grid{
     pub fn new(height:usize, width:usize)->Self{
-        let mut tmp: Vec<i16> = Vec::new();
+        let mut tmp: Vec<TileType> = Vec::new();
         tmp.reserve_exact(height*width);
         for _ in 0..width{
             for _ in 0..height{
                 tmp.push(-1);
             }
         }
-        let values:Box<[i16]> = tmp.into();
+        let values:Box<[TileType]> = tmp.into();
         return Self{values, height, width};
     }
 
-    pub fn get_sq(&self,x:usize, y:usize)->&i16{
+    pub fn get_sq(&self,x:usize, y:usize)->&TileType{
         assert!(x<self.width&& y<self.height);
         return &self.values[y*self.width+x];
     }
 
-    pub fn get_sqmut(&mut self, x:usize, y:usize)->&mut i16{
+    pub fn get_sqmut(&mut self, x:usize, y:usize)->&mut TileType{
         assert!(x<self.width&& y<self.height);
         return &mut self.values[y*self.width+x];
     }
@@ -53,21 +53,21 @@ impl Grid{
 #[derive(Clone)]
 pub struct GridConstraint{
     //returns true if the state is valid, false if it's invalid
-    pub constraints_sat:Arc<dyn Fn(&Grid,i16, usize, usize)->bool>,
+    pub constraints_sat:Arc<dyn Fn(&Grid,TileType, usize, usize)->bool>,
     pub debug_fn:Arc<dyn Fn()->String>,
 }
 impl std::fmt::Debug for GridConstraint{
     fn fmt(&self, formatter:&mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> { let _ = formatter.write_str(&(self.debug_fn)()); Ok(())}
 }
 impl GridConstraint{
-    pub fn new(func:Arc<dyn Fn(&Grid, i16,usize, usize)->bool>,debug_fn:Arc<dyn Fn()->String>)->Self{
+    pub fn new(func:Arc<dyn Fn(&Grid, TileType,usize, usize)->bool>,debug_fn:Arc<dyn Fn()->String>)->Self{
         return Self{constraints_sat:func, debug_fn}
     }
 
     // tile types; directions within tile tiles; allowed types per direction
-    pub fn new_from_borders(constraints:HashMap<i16,Vec<HashSet<i16>>>)->Self{
+    pub fn new_from_borders(constraints:HashMap<TileType,Vec<HashSet<TileType>>>)->Self{
         let debug_constraints = constraints.clone();
-        let func = move |grid:&Grid, tile_type:i16, x:usize, y:usize|{
+        let func = move |grid:&Grid, tile_type:TileType, x:usize, y:usize|{
             for i in 0..8{
                 let (dx,dy) = OFFSETS[i];
                 if x as isize+dx<0 || y as isize+dy<0{
@@ -99,13 +99,13 @@ impl GridConstraint{
     }
 
     //returns true if the state is valid, false if it's invalid
-    pub fn check_constraint(&self, grid:&Grid,test_value:i16, x:usize, y:usize)->bool{
+    pub fn check_constraint(&self, grid:&Grid,test_value:TileType, x:usize, y:usize)->bool{
         if test_value == -1{
             return true;
         }
         return (self.constraints_sat)(grid,test_value, x,y);
     }
-    pub fn get_allowed_states(&self,grid:&Grid,allowed_states:&[i16],x:usize, y:usize)->Vec<i16>{
+    pub fn get_allowed_states(&self,grid:&Grid,allowed_states:&[TileType],x:usize, y:usize)->Vec<TileType>{
         let mut out = vec![];
         for i in allowed_states{
             if self.check_constraint(grid, *i, x, y){
@@ -116,23 +116,23 @@ impl GridConstraint{
     }
 }
 
-pub enum SelectionStrategy{PurelyRandom, FromDistribution{distribution:Box<[(i16, f64)]>}, MinimizeEntropy, MaximizeEntropy}
+pub enum SelectionStrategy{PurelyRandom, FromDistribution{distribution:Box<[(TileType, f64)]>}, MinimizeEntropy, MaximizeEntropy}
 
 #[derive(Clone, Debug)]
 pub struct ConstraintSolver{
     grid:Grid, 
     constraints:Vec<GridConstraint>,
-    allowed_states:Arc<[i16]>, 
-    allowed_neighbors_cache:HashMap<(usize,usize), Vec<i16>>
+    allowed_states:Arc<[TileType]>, 
+    allowed_neighbors_cache:HashMap<(usize,usize), Vec<TileType>>
 }
 
 impl ConstraintSolver{
 
-    pub fn new(height:usize,width:usize, allowed_states:Arc<[i16]>)->Self{
+    pub fn new(height:usize,width:usize, allowed_states:Arc<[TileType]>)->Self{
         Self{grid:Grid::new(height, width), constraints:Vec::new(), allowed_states, allowed_neighbors_cache:HashMap::new()}
     }
 
-    pub fn new_with_constraints(height:usize, width:usize,allowed_states:Arc<[i16]>, constraints:Vec<GridConstraint>)->Self{
+    pub fn new_with_constraints(height:usize, width:usize,allowed_states:Arc<[TileType]>, constraints:Vec<GridConstraint>)->Self{
         Self{grid:Grid::new(height, width), constraints, allowed_states,allowed_neighbors_cache:HashMap::new()} 
     }
 
@@ -147,9 +147,9 @@ impl ConstraintSolver{
         return true;
     }
 
-    pub fn new_from_data(data:&[i16], height:usize, width:usize)->Self{
-        let mut used:HashSet<i16> = HashSet::new();
-        let mut allowed_states:Vec<i16> = Vec::new();
+    pub fn new_from_data(data:&[TileType], height:usize, width:usize)->Self{
+        let mut used:HashSet<TileType> = HashSet::new();
+        let mut allowed_states:Vec<TileType> = Vec::new();
         for i in data{
             if !used.contains(i){
                 used.insert(*i);
@@ -186,16 +186,17 @@ impl ConstraintSolver{
     }
 
     //sets the value at the location to the requested one, clears the cache of the value and all it's neighbors
-    pub unsafe fn collapse_unchecked(&mut self, x:usize, y:usize, value:i16){
+    pub unsafe fn collapse_unchecked(&mut self, x:usize, y:usize, value:TileType){
         *(self.grid.get_sqmut(x, y)) = value;
-        self.allowed_neighbors_cache.clear();
+        //self.allowed_neighbors_cache.clear();
     }
 
     //returns true on error
-    pub unsafe fn collapse_unchecked_recursive(&mut self, x:usize, y:usize, value:i16)->bool{
+    pub unsafe fn collapse_unchecked_recursive(&mut self, x:usize, y:usize, value:TileType)->bool{
         *(self.grid.get_sqmut(x, y)) = value;
         let ix = x as isize;
         let iy = y as isize;
+        self.allowed_neighbors_cache.remove(&(x,y));
         for i in 0..8{
             let dx = OFFSETS[i].0;
             let dy = OFFSETS[i].1;
@@ -206,6 +207,7 @@ impl ConstraintSolver{
             }
             let vx = sx as usize;
             let vy = sy as usize;
+            self.allowed_neighbors_cache.remove(&(vx,vy));
             if *(self.grid.get_sq(vx, vy)) != -1{
                 continue;
             }
@@ -221,7 +223,7 @@ impl ConstraintSolver{
         } 
         return false;
     }
-    pub fn check_collapse_allowed(&self, x:usize, y:usize, test_value:i16)->bool{
+    pub fn check_collapse_allowed(&self, x:usize, y:usize, test_value:TileType)->bool{
         for i in &self.constraints{
             if !i.check_constraint(&self.grid, test_value, x, y){
                 return false;
@@ -230,12 +232,16 @@ impl ConstraintSolver{
         return true;
     }
 
-    pub fn allowed_states_at(&self,x:usize, y:usize)->Vec<i16>{
+    pub fn allowed_states_at(&mut self,x:usize, y:usize)->Vec<TileType>{
+        if let Some(v) = self.allowed_neighbors_cache.get(&(x,y)){
+            return v.clone();
+        }
         let mut out = vec![];
         for i in &self.constraints{
             let mut tmp = i.get_allowed_states(&self.grid, &self.allowed_states, x, y);
             out.append(&mut tmp);
         }
+        self.allowed_neighbors_cache.insert((x,y), out.clone());
         return out;
     }
 
@@ -266,7 +272,7 @@ impl ConstraintSolver{
     }
 
     //returns Ok(true) if the collapse was allowed Ok(false) if collapsing reached an unreachable state returns an error
-    pub fn attempt_collapse_to_value(&mut self,x:usize, y:usize, value:i16)->Result<bool,()>{
+    pub fn attempt_collapse_to_value(&mut self,x:usize, y:usize, value:TileType)->Result<bool,()>{
         if self.check_collapse_allowed(x, y, value){
          
             let result =    unsafe{
@@ -347,7 +353,7 @@ impl ConstraintSolver{
                 }
             }
         }
-        idxs.sort_by(|a,b|{
+        idxs.sort_unstable_by(|a,b|{
             if a<b{return std::cmp::Ordering::Greater;} else{ return std::cmp::Ordering::Less}
             }
           );
@@ -359,7 +365,7 @@ impl ConstraintSolver{
     pub fn collapse_fully(&mut self, selection_mode:&SelectionStrategy)->bool{
         let mut ud_count = self.undefined_count();
         while ud_count>0{
-            println!("ud_count:{ud_count}");
+            //println!("ud_count:{ud_count}");
             let r = self.collapse_lowest_entropy(selection_mode);
             if r.is_err(){
                 return false;
@@ -371,9 +377,9 @@ impl ConstraintSolver{
             }
             ud_count = self.undefined_count();
         }
-        if !self.is_state_valid(){
+        /*if !self.is_state_valid(){
             return false;
-        }
+        }*/
         return true;
     }
 }
@@ -395,7 +401,7 @@ fn test_initial_state_is_valid(){
     let mut data =Vec::new();
     for i in 0..height{
         for j in 0..width{
-            data.push(if (i+j)%2 == 0{0 as i16} else{1 as i16});
+            data.push(if (i+j)%2 == 0{0 as TileType} else{1 as TileType});
         }
     }
     let mut solve=
@@ -410,12 +416,12 @@ fn test_initial_state_is_valid(){
 
 #[allow(unused)]
 pub fn test_collapse(){
-    let height:usize =100;
-    let width:usize = 100;
+    let height:usize =60;
+    let width:usize = 60;
     let mut data =Vec::new();
     for i in 0..height{
         for j in 0..width{
-            data.push(((i%2+j%3)) as i16);
+            data.push( ((thread_rng().next_u32())%3) as TileType);
         }
     }
     let mut solve=
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 pub mod constraints;
 mod utils;
+use std::time::SystemTime;
 fn main() {
+    let now = SystemTime::now();
     constraints::test_collapse();
+    match now.elapsed() {
+        Ok(elapsed) => {
+            // it prints '2'
+            println!("took {} ms", elapsed.as_millis());
+        }
+        Err(e) => {
+            // an error occurred!
+            println!("Error: {e:?}");
+        }
+    }
 }
